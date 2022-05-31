5994fb2699b68faf9bc6067bd9f73cebedf184b9
Coner Murphy <coner@conermurphy.com>
2022-05-31 21:40:07 +0100

    feat: setup example project

---
+++ components/Toggle.tsx
@@ -0,0 +1,30 @@
+import { useTheme } from 'next-themes';
+import React, { useEffect, useState } from 'react';
+
+export default function Toggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      onClick={() => {
+        if (theme === 'light') {
+          return setTheme('dark');
+        }
+        return setTheme('light');
+      }}
+      type="button"
+      className="opacity-75 p-6 text-xl"
+    >
+      Toggle to {theme === 'light' ? 'dark' : 'light'} theme
+    </button>
+  );
+}
